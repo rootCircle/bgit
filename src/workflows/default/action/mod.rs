e62b7d80c9525164f43a730d37a277b939f858ed
Sidharth-Singh10 <sidharthsingh714@gmail.com>
2025-06-08 01:34:37 +0530

    feat(ask_restore_changes): add multi_select

--- src/workflows/default/action/mod.rs
+++ src/workflows/default/action/mod.rs
@@ -2,7 +2,6 @@ pub(crate) mod ta01_is_git_repo;
 pub(crate) mod ta02_has_stash;
 pub(crate) mod ta03_pop_stash;
 pub(crate) mod ta04_has_unstaged;
-pub(crate) mod ta06_restore_changes;
 pub(crate) mod ta07_has_uncommitted;
 pub(crate) mod ta08_is_pulled_pushed;
 pub(crate) mod ta09_pull_push;
